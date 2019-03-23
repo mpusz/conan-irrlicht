b747dc62c7bd3b428547471e33e74f10589d213b
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-03-23 18:27:20 +0100

    Travis CI pagination support added to workaround 50 minutes timeout issue

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
 from cpt.packager import ConanMultiPackager
+from collections import defaultdict
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(curpage="x86", total_pages=2)
+    named_builds = defaultdict(list)
     builder.add_common_builds(shared_option_name="mypackagename:shared", pure_c=False)
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        named_builds[settings['arch']].append([settings, options, env_vars, build_requires, reference])
+    builder.named_builds = named_builds
     builder.run()
-    
