ba07d3e1127a5cc07dde3ae81116a98f31746b32
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-03-23 19:38:02 +0100

    Pagination changed

--- build.py
+++ build.py
@@ -1,11 +1,6 @@
 from cpt.packager import ConanMultiPackager
-from collections import defaultdict
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(curpage="x86", total_pages=2)
-    named_builds = defaultdict(list)
+    builder = ConanMultiPackager(curpage=1, total_pages=2)
     builder.add_common_builds(shared_option_name="mypackagename:shared", pure_c=False)
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        named_builds[settings['arch']].append([settings, options, env_vars, build_requires, reference])
-    builder.named_builds = named_builds
     builder.run()
