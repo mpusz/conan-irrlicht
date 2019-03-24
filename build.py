94a02720fa456a8643b4ba1708efc54e8f4837ee
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-03-24 16:43:21 +0100

    Conan pagination fixed

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(curpage=1, total_pages=2)
+    builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="irrlicht:shared", pure_c=False)
     builder.run()
