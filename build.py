af184e3fc721e91a2cb5f569f53bb9a9b2f4f456
Mateusz Pusz <mateusz.pusz@gmail.com>
2019-04-19 18:22:08 +0200

    Build scripts and configuration files refactored

--- build.py
+++ build.py
@@ -1,6 +1,12 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="irrlicht:shared", pure_c=False)
+    builder = ConanMultiPackager(username = "mpusz", login_username = "mpusz",
+                                 channel = "testing",
+                                 stable_branch_pattern = r"v\d+\.\d+\.\d+.*",
+                                 upload = "https://api.bintray.com/conan/mpusz/conan-mpusz",
+                                 remotes = None,
+                                 build_policy = None,
+                                 upload_dependencies=None)
+    builder.add_common_builds(pure_c=False)
     builder.run()
